08ab8861d812cf9c9bc1b747d58528517d7f6aa5
Aryan Solanki <80894090+aryan-2059@users.noreply.github.com>
2025-11-11 21:11:18 +0530

    Implement file saving for calorie summary
    Added functionality to save calorie summary to a file with a timestamp.

--- lab_1.py
+++ lab_1.py
@@ -3,6 +3,7 @@
 #2501730347
 #Date: 28 october 2025
 # calorie tracker
+import datetime
 print('''***** WELCOME TO CALORIE TRACKER *****
       You can track your daily calorie intake here.''')
 n = int(input("Enter number of meals:\t"))
@@ -30,10 +31,32 @@ print(f"\t\tTotal:\t{total_calories}")
 print(f"\t\tAverage per meal:\t{avg_cal}")
 print("\t\t-------------------------")
 
-if total_calories > lim:
+if avg_cal > lim:
     print('''***WARNING***
           YOU HAVE EXCEEDED YOUR DAILY CALORIE LIMIT!!!''')
 else: 
     print("You are within your daily calorie limit :)")
 print(total_calories)
 
+wish = input("Do you want to save this summary to a file? (yes/no):\t").lower()
+if wish == 'yes':
+    filename = 'data.txt'
+    timestamp= datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+    with open(filename, 'a') as file:
+        file.write(f"\n***** DAILY CALORIE INTAKE SUMMARY ({timestamp}) *****\n")
+        file.write("\t\tMeal Name\tCalories\n")
+        file.write("\t\t-------------------------\n")
+        for i in range(n):
+            file.write(f"\t\t{meals[i]}\t{calories[i]}\n")
+        file.write(f"\t\tTotal:\t{total_calories}\n")
+        file.write(f"\t\tAverage per meal:\t{avg_cal}\n")
+        file.write("\t\t-------------------------\n")
+        if total_calories > lim:
+            file.write('''***WARNING***
+          YOU HAVE EXCEEDED YOUR DAILY CALORIE LIMIT!!!\n''')
+        else:
+            file.write("You are within your daily calorie limit :)\n")
+    print(f"Summary saved to {filename}")
+else:
+    print("Summary not saved.")
